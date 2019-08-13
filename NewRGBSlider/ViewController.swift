fffa25cab13a30a21e14d1774bfac08ab0d2fd07
Bogdan Baskakov <bgdnbskkv@gmail.com>
2019-08-13 12:51:01 +0500

    Initial Commit

---
+++ NewRGBSlider/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewRGBSlider
+//
+//  Created by Bogdan Baskakov on 13/08/2019.
+//  Copyright © 2019 Bogdan Baskakov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
